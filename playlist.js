5c24e60a82ad39189daa21dbbfda96705250d9a2
ricap18 <120043393+ricap18@users.noreply.github.com>
2023-04-16 13:58:27 +0200

    Terminado

---
+++ playlist.js
@@ -0,0 +1,35 @@
+[{
+  "_id": {
+    "$oid": "643bd993cf2c248f2f043b7e"
+  },
+  "id_usuario": "643bc051cf2c248f2f043b6f  ",
+  "videos": [
+    {
+      "id_video": "643bc8a6cf2c248f2f043b72 ",
+      "nombre": " "
+    },
+    {
+      "id_video": "643bc8a6cf2c248f2f043b73 ",
+      "nombre": " "
+    }
+  ],
+  "estado": " privado ",
+  "nombre": " "
+},{
+  "_id": {
+    "$oid": "643bd993cf2c248f2f043b7f"
+  },
+  "id_usuario": "643bc051cf2c248f2f043b70  ",
+  "videos": [
+    {
+      "id_video": "643bc8a6cf2c248f2f043b72 ",
+      "nombre": " "
+    },
+    {
+      "id_video": " ",
+      "nombre": " "
+    }
+  ],
+  "estado": " publico ",
+  "nombre": " "
+}]
